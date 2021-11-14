258c0ea13807f726b1bfb2b5489a8d24e6d643cd
Vandermaesen Pierre <vandermaesenpi@gmail.com>
2021-11-14 22:27:23 +0100

    {fix} missing virtual in Wall script

--- Splitempo Unity Project/Assets/Scripts/Gameplay/Interactables/Wall.cs
+++ Splitempo Unity Project/Assets/Scripts/Gameplay/Interactables/Wall.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Wall : MonoBehaviour, IInteractable
 {
-    public void Interact(BallMovement interactor, RaycastHit hit)
+    public virtual void Interact(BallMovement interactor, RaycastHit hit)
     {
         interactor.Reflect(hit.normal);
     }
